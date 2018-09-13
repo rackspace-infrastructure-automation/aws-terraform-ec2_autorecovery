4378413f9c80d0ede75d9fdb720a0edb98f83e73
mjcmtb <mikejnc@gmail.com>
2018-09-13 13:25:17 -0700

    Reaper file/configuration sync update 09/13/2018 13:25:17

--- .circleci/bin/plan.sh
+++ .circleci/bin/plan.sh
@@ -9,18 +9,19 @@ LAYERS_DIR="$WORKING_DIR/layers"
 if [ -f "$WORKSPACE_DIR/changed_layers" ]; then
   LAYERS=$(cat "$WORKSPACE_DIR/changed_layers")
 else
-  LAYERS=$(find "$LAYERS_DIR"/* -type d -maxdepth 0 -exec basename '{}' \; | sort -n)
+  LAYERS=$(find "$LAYERS_DIR"/* -maxdepth 0 -type d -exec basename '{}' \; | sort -n)
 fi
 
 for LAYER in $LAYERS; do
   echo "terraform init $LAYER"
-  (cd "$LAYERS_DIR/$LAYER" && terraform init -input=false -no-color)
+  (cd "$LAYERS_DIR/$LAYER" && terraform init -force-copy --input=false -no-color)
 
   # cache .terraform during the plan
   (cd "$LAYERS_DIR/$LAYER" && tar -czf "$WORKSPACE_DIR/.terraform.$LAYER.tar.gz" .terraform)
 
   echo "terraform plan $LAYER"
-  (cd "$LAYERS_DIR/$LAYER" && terraform plan -no-color -input=false -out="$WORKSPACE_DIR/terraform.$LAYER.plan" | tee "$WORKSPACE_DIR/full_plan_output.log" | grep -v "Refreshing state" )
+  (cd "$LAYERS_DIR/$LAYER" && cat /dev/null > "layerplan.log" && terraform plan -no-color -input=false -out="$WORKSPACE_DIR/terraform.$LAYER.plan" | tee "layerplan.log" | grep -v "Refreshing state" )
+  (cd "$LAYERS_DIR/$LAYER" && cat "layerplan.log" >> "$WORKSPACE_DIR/full_plan_output.log")
 
   # for debugging, show these files exist
   ls -la "$WORKSPACE_DIR/.terraform.$LAYER.tar.gz"
