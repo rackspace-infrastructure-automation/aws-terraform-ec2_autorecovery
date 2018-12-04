0b4b6cf572c635fe28a79bb6679ec9043380ba90
Danny Smith <danny.smith@rackspace.co.uk>
2018-12-04 15:48:26 +0000

    Add IP outputs to module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "ar_instance_id_list" {
   description = "List of resulting Instance IDs"
   value       = ["${split(",",var.secondary_ebs_volume_size != "" ? join(",",coalescelist(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, list("novalue"))) : join(",",coalescelist(aws_instance.mod_ec2_instance_no_secondary_ebs.*.id, list("novalue"))))}"]
 }
+
+output "ar_instance_ip_list" {
+  description = "List of resulting Instance IP addresses"
+  value       = ["${split(",",var.secondary_ebs_volume_size != "" ? join(",",coalescelist(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, list("novalue"))) : join(",",coalescelist(aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip, list("novalue"))))}"]
+}
+
+output "ar_instance_id_ip_zipmap" {
+  description = "Map of resulting Instance IDs to Instance IP addresses"
+  value       = "${zipmap(output.ar_instance_id_list, output.ar_instance_ip_list)}"
+}
