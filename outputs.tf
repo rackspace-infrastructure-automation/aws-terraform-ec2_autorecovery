72bdd9eaf4325d21d0322411d01d5c43ce5fc9fa
Danny Smith <danny.smith@rackspace.co.uk>
2018-12-04 18:30:41 +0000

    Remove zipmap of IDs and IPs, not working and not required

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,3 @@ output "ar_instance_ip_list" {
 
   value = "${concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip)}"
 }
-
-output "ar_instance_id_ip_zipmap" {
-  description = "Map of resulting Instance IDs to Instance IP addresses"
-  value       = "${zipmap(concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, aws_instance.mod_ec2_instance_no_secondary_ebs.*.id), concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip))}"
-}
