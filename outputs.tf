afda8e06870a4695077b7ca3817669d1e761919a
Danny Smith <danny.smith@rackspace.co.uk>
2018-12-04 16:31:53 +0000

    Change ID list to match previous commit to test on personal build

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,10 @@
 output "ar_instance_id_list" {
   description = "List of resulting Instance IDs"
-  value       = ["${split(",",var.secondary_ebs_volume_size != "" ? join(",",coalescelist(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, list("novalue"))) : join(",",coalescelist(aws_instance.mod_ec2_instance_no_secondary_ebs.*.id, list("novalue"))))}"]
+
+  value = [
+    "${aws_instance.mod_ec2_instance_with_secondary_ebs.*.id}",
+    "${aws_instance.mod_ec2_instance_no_secondary_ebs.*.id}",
+  ]
 }
 
 output "ar_instance_ip_list" {
