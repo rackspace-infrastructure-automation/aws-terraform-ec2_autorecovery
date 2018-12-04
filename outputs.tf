e99a0021f60e39a5a69983ea2f29dd4ec250e663
Danny Smith <danny.smith@rackspace.co.uk>
2018-12-04 16:48:55 +0000

    Update output lists to correct formatting as suggested by John Titus

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,16 @@
 output "ar_instance_id_list" {
   description = "List of resulting Instance IDs"
 
-  value = [
-    "${aws_instance.mod_ec2_instance_with_secondary_ebs.*.id}",
-    "${aws_instance.mod_ec2_instance_no_secondary_ebs.*.id}",
-  ]
+  value = "${concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, aws_instance.mod_ec2_instance_no_secondary_ebs.*.id)}"
 }
 
 output "ar_instance_ip_list" {
   description = "List of resulting Instance IP addresses"
 
-  value = [
-    "${aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip}",
-    "${aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip}",
-  ]
+  value = "${concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip)}"
 }
 
 output "ar_instance_id_ip_zipmap" {
   description = "Map of resulting Instance IDs to Instance IP addresses"
-  value       = "${zipmap(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, aws_instance.mod_ec2_instance_no_secondary_ebs.*.id)}"
+  value       = "${zipmap(concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, aws_instance.mod_ec2_instance_no_secondary_ebs.*.id), concat(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip))}"
 }
