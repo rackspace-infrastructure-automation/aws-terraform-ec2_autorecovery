3d4fcdbed68db709c5275a06487833d248c68a9f
Danny Smith <danny.smith@rackspace.co.uk>
2018-12-04 16:21:34 +0000

    Change IP list and ID/IP zipmap to use direct counted output

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,14 @@ output "ar_instance_id_list" {
 
 output "ar_instance_ip_list" {
   description = "List of resulting Instance IP addresses"
-  value       = ["${split(",",var.secondary_ebs_volume_size != "" ? join(",",coalescelist(aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip, list("novalue"))) : join(",",coalescelist(aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip, list("novalue"))))}"]
+
+  value = [
+    "${aws_instance.mod_ec2_instance_with_secondary_ebs.*.private_ip}",
+    "${aws_instance.mod_ec2_instance_no_secondary_ebs.*.private_ip}",
+  ]
 }
 
 output "ar_instance_id_ip_zipmap" {
   description = "Map of resulting Instance IDs to Instance IP addresses"
-  value       = "${zipmap(output.ar_instance_id_list, output.ar_instance_ip_list)}"
+  value       = "${zipmap(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, aws_instance.mod_ec2_instance_no_secondary_ebs.*.id)}"
 }
