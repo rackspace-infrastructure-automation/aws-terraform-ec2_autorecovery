0d82a3a9ad1dd01ec205b3a122c79cf0cad2feff
bohn002 <nath@nbohn.com>
2019-06-26 09:41:44 -0500

    increment versions

--- examples/main.tf
+++ examples/main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "amazon_centos_7" {
 }
 
 module "ec2_ar" {
-  source                       = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=v0.0.17"
+  source                       = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=v0.0.19"
   ec2_os                       = "centos7"
   instance_count               = "3"
   subnets                      = "${module.vpc.public_subnets}"
